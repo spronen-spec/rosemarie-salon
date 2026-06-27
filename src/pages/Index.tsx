6fa6045080b3dc35a4f41f3b43035ad6076f0d5d
Ronen <sp.ronen@gmail.com>
2026-06-27 17:25:43 +0400

    Remove Instagram section, move About to bottom of page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import ServicesSection from "@/components/ServicesSection";
-import AboutSection from "@/components/AboutSection";
 import GallerySection from "@/components/GallerySection";
 import TeamSection from "@/components/TeamSection";
 import ReviewsSection from "@/components/ReviewsSection";
-import InstagramSection from "@/components/InstagramSection";
 import FAQSection from "@/components/FAQSection";
 import ParkingSection from "@/components/ParkingSection";
 import CtaSection from "@/components/CtaSection";
+import AboutSection from "@/components/AboutSection";
 import Footer from "@/components/Footer";
 import WhatsAppFab from "@/components/WhatsAppFab";
 
@@ -18,14 +17,13 @@ const Index = () => {
       <Navbar />
       <Hero />
       <ServicesSection />
-      <AboutSection />
       <GallerySection />
       <TeamSection />
       <ReviewsSection />
-      <InstagramSection />
       <FAQSection />
       <ParkingSection />
       <CtaSection />
+      <AboutSection />
       <Footer />
       <WhatsAppFab />
     </div>
